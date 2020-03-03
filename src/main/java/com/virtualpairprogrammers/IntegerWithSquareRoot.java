a29fa96cad39bf72a9041eb04d9d2ea7151adcdd
Adam Łagoda <e-amla@gft.com>
2020-03-03 13:24:28 +0100

    Initial commit

---
+++ src/main/java/com/virtualpairprogrammers/IntegerWithSquareRoot.java
@@ -0,0 +1,12 @@
+package com.virtualpairprogrammers;
+
+public class IntegerWithSquareRoot {
+
+    private int originalNumber;
+    private double squareRoot;
+
+    public IntegerWithSquareRoot(int i) {
+        originalNumber = i;
+        squareRoot = Math.sqrt(i);
+    }
+}
---
+++ src/main/java/com/virtualpairprogrammers/Main.java
@@ -0,0 +1,32 @@
+package com.virtualpairprogrammers;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> inputData = new ArrayList<>();
+        inputData.add("WARN: Tuesday 4 September 0405");
+        inputData.add("ERROR: Tuesday 4 September 0408");
+        inputData.add("FATAL: Wednesday 5 September 1632");
+        inputData.add("ERROR: Friday 7 September 1854");
+        inputData.add("WARN: Saturday 8 September 1942");
+
+        Logger.getLogger("org.apache").setLevel(Level.WARN);
+
+        SparkConf conf = new SparkConf().setAppName("Starting Spark").setMaster("local[*]");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<String> originalLogMessages = sc.parallelize(inputData);
+
+        sc.close();
+    }
+}
